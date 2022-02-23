d6dd7d4e4404ae60eb38e9f516877468930bac44
michael villaronga <michael.villaronga@gmail.com>
2022-02-22 17:01:51 -0800

    Fix removed half block highlight

--- src/main/java/net/pirateyoda/aabba/blocks/BarrelBlock.java
+++ src/main/java/net/pirateyoda/aabba/blocks/BarrelBlock.java
@@ -3,7 +3,10 @@ package net.pirateyoda.aabba.blocks;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -23,9 +26,6 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.pirateyoda.aabba.blockentity.BarrelBlockEntity;
 import org.jetbrains.annotations.Nullable;
@@ -45,18 +45,6 @@ public class BarrelBlock extends HorizontalFacingBlock implements BlockEntityPro
         stateManager.add(Properties.HORIZONTAL_FACING);
     }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext ctx) {
-        Direction dir = state.get(FACING);
-        return switch (dir) {
-            case NORTH -> VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
-            case SOUTH -> VoxelShapes.cuboid(0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f);
-            case EAST -> VoxelShapes.cuboid(0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-            case WEST -> VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 0.5f, 1.0f, 1.0f);
-            default -> VoxelShapes.fullCube();
-        };
-    }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
